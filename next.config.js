d4ee1cfd9508903287e52688a16ca2f46ae2005d
terracotta <begaiym.akylova@gmail.com>
2025-04-14 12:39:53 +0600

    add env in nextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['frontend-test-api.yoldi.agency'],
   },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 }
 
 module.exports = nextConfig
